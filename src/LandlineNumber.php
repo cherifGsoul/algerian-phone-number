ac12d9e2c7e50628bdd7770ff082ee71c82a051a
Nasser Abachi <abachinasser@gmail.com>
2020-12-06 20:57:56 +0100

    remove duplicated landline province prefixes

--- src/LandlineNumber.php
+++ src/LandlineNumber.php
@@ -10,7 +10,7 @@ final class LandlineNumber extends AbstractAlgerianPhoneNumber
 {
     protected function setNumber(string $number)
     {
-        $pattern = '/^(00213|\+213|0)(49|27|29|32|33|34|33|49|25|26|29|37|43|46|26|21|23|27|34|36|48|39|48|38|37|31|25|45|35|45|29|41|49|29|35|24|38|46|32|37|24|31|27|49|43|29|46)[0-9]{6}/';
+        $pattern = '/^(00213|\+213|0)(49|27|29|32|33|34|25|26|37|43|46|21|23|36|48|39|38|31|45|35|41|24)[0-9]{6}/';
         if (!preg_match($pattern, $number)) {
             throw new InvalidLandlineNumberException('The landline phone number is invalid!');
         }
